851563e5219bcac0488669cf911a373edb0e09d8
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-01-04 20:02:17 +0200

    connect with mongoose

--- src/server.js
+++ src/server.js
@@ -1,6 +1,21 @@
 const app = require('./app');
+const mongoose = require('mongoose');
+const config = require('./config/env.config');
 
-const port = process.env.PORT || 4000;
+const DB = config.database;
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then(() => {
+        console.log('DB connection successful!');
+    });
+
+const port = config.port || 4000;
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);

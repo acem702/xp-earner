0d25cbb9fc25dfd5da95c4ccd11c91b283e05344
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-01-06 19:23:23 +0200

    + add user completeTask controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 const factory = require('./handlerFactory');
 const User = require('../models/user.model');
+const Task = require('../models/task.model');
 const catchAsync = require('../utils/catchAsync.util');
+const AppError = require('../utils/AppError.util');
 
 exports.getAllUsers = factory.getAll(User);
 exports.getUser = factory.getOne(User, { path: 'tasks' });
@@ -15,6 +17,45 @@ exports.createUser = catchAsync(async (req, res, next) => {
     });
 
     res.status(201).json({
+        status: 'success',
+        data: {
+            data: user,
+        },
+    });
+});
+
+// *** user complete a task ***
+exports.completeTask = catchAsync(async (req, res, next) => {
+    // [1] get task id from req.params
+    const { taskId } = req.params;
+    // [2] get user id from req.user
+    const userId = req.user.id;
+
+    // [3] find task info by id
+    const task = await Task.findById(taskId);
+
+    // [4] check if the user completed the task before
+
+    const user = await User.findById(userId);
+
+    const taskExist = user.completed_tasks.some(
+        (el) => el.task_id.toString() === taskId.toString(),
+    );
+
+    if (taskExist) {
+        return next(new AppError('You already completed this task', 400));
+    }
+
+    // [5] if not completed before, add task to user.completed_tasks and increase xp_points
+    user.completed_tasks.push({
+        task_id: taskId,
+    });
+
+    user.xp_points += task.xp_points;
+
+    await user.save({ validateBeforeSave: false });
+
+    res.status(200).json({
         status: 'success',
         data: {
             data: user,

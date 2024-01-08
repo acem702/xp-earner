e9463dca3abd72c414e86dec463eb70fccaa3caf
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-01-08 06:49:27 +0200

    + add useEffect hook to get tasks

--- client/src/pages/tasks/Tasks.js
+++ client/src/pages/tasks/Tasks.js
@@ -1,4 +1,24 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
 const Tasks = () => {
+
+
+    useEffect(() => {
+
+        axios.get('http://localhost:4000/api/v1/tasks',
+        {
+            withCredentials: true,
+            credentials: 'include'
+        }
+        ).then((res) => {
+            console.log('data', res);
+            // get user from locals user
+        }).catch((err) => {
+            console.log(err);
+        });
+    },[])
     return (
         <div>
             <h1>Tasks</h1>

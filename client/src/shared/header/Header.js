3c7e935b1aed9e1c5073864f2f8f5353cfddede9
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-01-08 06:50:45 +0200

    + view header based is user or not

--- client/src/shared/header/Header.js
+++ client/src/shared/header/Header.js
@@ -3,9 +3,27 @@ import { Navbar, Nav, Button } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons'; // Import the user icon
+import axios from 'axios';
+import isLogged from '../../services/auth';
 
 const AppHeader = () => {
+    const isUser = isLogged()
+
     const navigate = useNavigate();
+
+    const handleLogout = () => {
+        axios.get('http://localhost:4000/api/v1/logout', {
+            withCredentials: true,
+            credentials: 'include'
+        }).then((res) => {
+            console.log(res);
+            navigate('/');
+            alert('Logged out successfully');
+        }).catch((err) => {
+            console.log(err);
+            alert(err.response.data.message);
+        });
+    }
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Navbar.Brand
@@ -19,22 +37,30 @@ const AppHeader = () => {
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto"></Nav>
                 <Nav>
-                    <Link to="/login" className="nav-link">
-                        Login
-                    </Link>
-                    <Link to="/register" className="nav-link">
-                        Sign Up
-                    </Link>
-                    <Link to="/profile" className="nav-link">
-                        <FontAwesomeIcon
-                            icon={faUser}
-                            style={{ marginRight: '5px' }}
-                        />
-                        Profile
-                    </Link>
-                    <Link to="/">
-                        <Button variant="danger">Logout</Button>
-                    </Link>
+                    {!isUser &&
+                        <Link to="/login" className="nav-link">
+                            Login
+                        </Link>                
+                    }
+                    {!isUser &&
+                        <Link to="/register" className="nav-link">
+                            Sign Up
+                        </Link>
+                    }
+                    {isUser &&
+                        <Link to="/profile" className="nav-link">
+                            <FontAwesomeIcon
+                                icon={faUser}
+                                style={{ marginRight: '5px' }}
+                                />
+                            Profile
+                        </Link>
+                    }
+                    {isUser &&
+                        <Link to="/">
+                            <Button onClick={handleLogout} variant="danger">Logout</Button>
+                        </Link>
+                    }
                 </Nav>
             </Navbar.Collapse>
         </Navbar>

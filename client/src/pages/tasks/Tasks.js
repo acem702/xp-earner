f4f3b1c83207362c974e5d6d1436029d6f82db2e
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-01-08 07:35:25 +0200

    + connect landing page with BE

--- client/src/pages/tasks/Tasks.js
+++ client/src/pages/tasks/Tasks.js
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
-import Cookies from 'js-cookie';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Button } from 'react-bootstrap';
+const { useNavigate } = require("react-router-dom");
 
 const Tasks = () => {
 
+    const navigate = useNavigate();
+
+    const [tasks, setTasks] = useState([]);
 
     useEffect(() => {
-
         axios.get('http://localhost:4000/api/v1/tasks',
         {
             withCredentials: true,
@@ -14,15 +18,32 @@ const Tasks = () => {
         }
         ).then((res) => {
             console.log('data', res);
-            // get user from locals user
+            setTasks(res.data.data.data);
         }).catch((err) => {
+            alert(err.response.data.message);
             console.log(err);
         });
     },[])
     return (
-        <div>
-            <h1>Tasks</h1>
+        <div className="container mt-5">
+        <h2>Complete Tasks to Earn XP</h2>
+        <div className="row">
+          {tasks && tasks.length > 0 && tasks.map((task, i) => (
+            <div key={task.id} className="col-lg-4 mb-3">
+              <Card>
+                <Card.Body>
+                  <Card.Title>{task.name}</Card.Title>
+                  <Card.Text>{task.description}</Card.Text>
+                  <Card.Text>XP Points: {task.xp_points}</Card.Text>
+                  <Button onClick={() => {
+                        navigate(`/task/${task.slug}`);
+                  }} variant="primary">View Task</Button>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
         </div>
+      </div>
     );
 };
 

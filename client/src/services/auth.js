9592fb17801ebcf02ac1122118c1d1db13c6a1a3
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-01-08 06:50:22 +0200

    + add isLogged function

---
+++ client/src/services/auth.js
@@ -0,0 +1,19 @@
+import {jwtDecode} from 'jwt-decode';
+import Cookies from 'js-cookie';
+
+const isLogged = () => {
+    const token = Cookies.get('JWT');
+
+    if(token === 'loggedout') return false;
+
+    if (!token) return false;
+    const decodedToken = jwtDecode(token);
+    const now = Date.now() / 1000;
+    if (decodedToken.exp < now) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+export default isLogged;

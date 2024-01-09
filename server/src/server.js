4aeeb6077f3a591b099c9ba59eaf8b5d0b30c912
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-01-09 19:33:36 +0200

    + add common error handling

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,12 @@ const app = require('./app');
 const mongoose = require('mongoose');
 const config = require('./config/env.config');
 
+process.on('uncaughtException', (err) => {
+    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
 const DB = config.database;
 
 mongoose
@@ -17,6 +23,21 @@ mongoose
 
 const port = config.port || 4000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+    console.log('Unhandled rejection 💥 Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
+process.on('SIGTERM', () => {
+    console.log('SIGTERM RECEIVED. Shutting down gracefully');
+    server.close(() => {
+        console.log('Process terminated!');
+    });
 });

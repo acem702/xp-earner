bdc5a28c471f84bc5b1c8f1d067182bbdcd5612d
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-01-11 00:56:45 +0200

    + add putLastActivity middleware

--- server/src/routes/index.route.js
+++ server/src/routes/index.route.js
@@ -2,11 +2,15 @@ const express = require('express');
 const taskRoutes = require('./APIs/task.route');
 const userRoutes = require('./APIs/user.route');
 const authRoutes = require('./APIs/auth.route');
+const authController = require('../controllers/auth.controller');
 
 const router = express.Router();
 
+router.use('/', authRoutes);
+
+router.use(authController.putLastActivity);
+
 router.use('/tasks', taskRoutes);
 router.use('/users', userRoutes);
-router.use('/', authRoutes);
 
 module.exports = router;

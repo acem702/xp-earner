681d13688ddcd6349dec72353a3db8211771b049
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-01-11 06:39:16 +0200

    + use upload image middleware when user signup

--- server/src/routes/APIs/auth.route.js
+++ server/src/routes/APIs/auth.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const authController = require('../../controllers/auth.controller');
+const upload = require('../../utils/upload.util');
 
 const router = express.Router();
 
-router.post('/signup', authController.signup);
+router.post('/signup', upload.single('image'), authController.signup);
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
 

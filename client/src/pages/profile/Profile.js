dd6006bcc6606103efa37aea40009f9abbc6081b
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-01-11 06:42:38 +0200

    + view image im user profile page

--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -1,85 +1,107 @@
 import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card } from 'react-bootstrap';
+import { Card, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
 
 const Profile = () => {
-    
     const [user, setUser] = useState({});
-  
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-
-        axios.get('http://localhost:4000/api/v1/users/me', {
-            withCredentials: true,
-            credentials: 'include'
-        }).then((res) => {
-            console.log(res);
-            setUser(res.data.data.data);
-        }).catch((err) => {
-            console.log(err);
-            alert(err.response.data.message);
-        });
+        setLoading(true);
+        axios
+            .get('http://localhost:4000/api/v1/users/me', {
+                withCredentials: true,
+                credentials: 'include',
+            })
+            .then((res) => {
+                console.log(res);
+                setUser(res.data.data.data);
+                setLoading(false);
+            })
+            .catch((err) => {
+                setLoading(false);
+                console.log(err);
+                setError(err.response.data.message);
+            });
     }, []);
-//     const user = {
-//     name: 'John Doe',
-//     email: 'john@example.com',
-//     xpPoints: 100,
-//     finishedTasks: [
-//       {
-//         id: 1,
-//         name: 'Task 1',
-//         description: 'Description for Task 1',
-//         xpPoints: 10,
-//       },
-//       {
-//         id: 2,
-//         name: 'Task 2',
-//         description: 'Description for Task 2',
-//         xpPoints: 15,
-//       },
-//       // Add more finished tasks as needed
-//     ],
-//   };
 
-  return (
-    <div className="container mt-5">
-      <h2>Profile</h2>
-      <Card>
-        <Card.Body>
-          <Card.Title>User Information</Card.Title>
-          <Card.Text>Name: {user.name}</Card.Text>
-          <Card.Text>Email: {user.email}</Card.Text>
-          <Card.Text>XP Points: {user.xp_points}</Card.Text>
-        </Card.Body>
-      </Card>
-
-      <h3 className="mt-4">Finished Tasks</h3>
-
-      {user.completed_tasks && user.completed_tasks.length === 0 && (
-
-        <h6>You have not completed any tasks yet.</h6>
-        )
-
-      }
-      <div className="row">
-        
-        {user.completed_tasks && user.completed_tasks.length > 0  && (
-          user.completed_tasks.map((task, i) => (
-            <div key={i} className="col-lg-4 mb-3">
-              <Card>
-                <Card.Body>
-                  <Card.Title>{task.task_id.name}</Card.Title>
-                  <Card.Text>{task.task_id.description}</Card.Text>
-                  <Card.Text>XP Points: {task.task_id.xp_points}</Card.Text>
-                </Card.Body>
-              </Card>
+    // spinner while loading functionality
+    if (loading) {
+        return (
+            <div className="container mt-5">
+                <h2>Loading...</h2>
             </div>
-          ))
-        ) }
-      </div>
-    </div>
-  );
+        );
+    }
+
+    // error handling functionality
+    if (error) {
+        return (
+            <div className="container mt-5">
+                <h2>{error}</h2>
+            </div>
+        );
+    }
+
+    return (
+        <div className="container mt-5">
+            <h2>Profile</h2>
+            <Row>
+                <Col lg={8}>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>User Information</Card.Title>
+                            <Card.Text>Name: {user.name}</Card.Text>
+                            <Card.Text>Email: {user.email}</Card.Text>
+                            <Card.Text>XP Points: {user.xp_points}</Card.Text>
+                        </Card.Body>
+                    </Card>
+
+                    <h3 className="mt-4">Finished Tasks</h3>
+
+                    {user.completed_tasks &&
+                        user.completed_tasks.length === 0 && (
+                            <h6>You have not completed any tasks yet.</h6>
+                        )}
+                    <div className="row">
+                        {user.completed_tasks &&
+                            user.completed_tasks.length > 0 &&
+                            user.completed_tasks.map((task, i) => (
+                                <div key={i} className="col-lg-4 mb-3">
+                                    <Card>
+                                        <Card.Body>
+                                            <Card.Title>
+                                                {task.task_id.name}
+                                            </Card.Title>
+                                            <Card.Text>
+                                                {task.task_id.description}
+                                            </Card.Text>
+                                            <Card.Text>
+                                                XP Points:{' '}
+                                                {task.task_id.xp_points}
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </div>
+                            ))}
+                    </div>
+                </Col>
+                <Col lg={4}>
+                    {/* Displaying Avatar/Profile Image */}
+                    {user.avatar && (
+                        <img
+                            src={`http://localhost:4000/${user.avatar}`}
+                            alt="Profile Avatar"
+                            className="mb-3"
+                            style={{ maxWidth: '100%' }}
+                        />
+                    )}
+                </Col>
+            </Row>
+        </div>
+    );
 };
 
 export default Profile;

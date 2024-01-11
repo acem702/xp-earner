be82ff8cf4bcfb515093545c26e72cdde0c24260
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-01-11 06:40:57 +0200

    + add function retun token data

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -1,10 +1,10 @@
-import jwt,{jwtDecode} from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 import Cookies from 'js-cookie';
 
-const isLogged = () => {
+export const isLogged = () => {
     const token = Cookies.get('JWT');
 
-    if(token === 'loggedout') return false;
+    if (token === 'loggedout') return false;
 
     if (!token) return false;
     const decodedToken = jwtDecode(token);
@@ -17,6 +17,22 @@ const isLogged = () => {
     } else {
         return true;
     }
-}
+};
 
-export default isLogged;
+export const tokenData = () => {
+    const token = Cookies.get('JWT');
+
+    if (token === 'loggedout') return false;
+
+    if (!token) return false;
+    const decodedToken = jwtDecode(token);
+
+    if (!decodedToken) return false;
+
+    const now = Date.now() / 1000;
+    if (decodedToken.exp < now) {
+        return false;
+    } else {
+        return decodedToken;
+    }
+};

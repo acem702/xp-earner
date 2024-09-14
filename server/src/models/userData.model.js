cefbcf78912cff523e764e928527b1c075fb3a80
acem702 <55085012+acem702@users.noreply.github.com>
2024-09-14 13:04:54 +0100

    Update userData.model.js

--- server/src/models/userData.model.js
+++ server/src/models/userData.model.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 const userDataSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
+  userId: { type: String, unique: true, required: true },
   balance: { type: Number, required: true }
 });
 
